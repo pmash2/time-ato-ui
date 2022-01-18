dc5b6496de1a8541821d22bcae1720c39868edfa
Phil Ashton <phillipashton@quadax.com>
2022-01-17 22:37:12 -0500

    Return null instead of attempting to build hub connection with missing configs

--- src/helpers/hubConnectionHelper.ts
+++ src/helpers/hubConnectionHelper.ts
@@ -3,13 +3,18 @@ import { getConfig } from "../helpers/app-config-helper"
 
 export const getHubConnection = (hubName: string): HubConnection | null => {
 	const _configs = getConfig()
-	const notificationHubUrl = `${_configs.NotificationsUrl}/hubs/${hubName}`
 
-	return new HubConnectionBuilder()
-		.withUrl(notificationHubUrl, {
-			skipNegotiation: true,
-			transport: HttpTransportType.WebSockets,
-		})
-		.withAutomaticReconnect()
-		.build()
+	if (_configs.NotificationsUrl.length > 0) {
+		const notificationHubUrl = `${_configs.NotificationsUrl}/hubs/${hubName}`
+
+		return new HubConnectionBuilder()
+			.withUrl(notificationHubUrl, {
+				skipNegotiation: true,
+				transport: HttpTransportType.WebSockets,
+			})
+			.withAutomaticReconnect()
+			.build()
+	} else {
+		return null
+	}
 }
